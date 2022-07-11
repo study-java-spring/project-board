24d57c815ad889e5b5b4c8ec3f14aed010d854c8
Xiah <xiah0526@gmail.com>
2022-07-11 11:52:57 +0900

    #8 - 도메인 코드를 엔티티로 변환
    1. 도메인 클래스 == 1 테이블 매칭되도록 설계
    * 생성자와 equals(), hashCode() 등이 도메인 맥락에 맞게끔 디테일하게 설정

--- src/main/java/com/study/board/domain/Article.java
+++ src/main/java/com/study/board/domain/Article.java
@@ -1,20 +1,75 @@
 package com.study.board.domain;
 
 
-import javax.persistence.Entity;
+import lombok.*;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.LinkedHashSet;
+import java.util.Objects;
+import java.util.Set;
 
 @Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@ToString
+@EntityListeners(AuditingEntityListener.class)
+@Table(indexes = {
+        @Index(columnList = "title"),
+        @Index(columnList = "hashTag"),
+        @Index(columnList = "createdAt"),
+        @Index(columnList = "createdBy"),
+})
 public class Article {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String title;
-    private String content;
-    private String hashTag;
 
-    private LocalDateTime createdAt;
-    private String createdBy;
-    private LocalDateTime modifiedAt;
-    private String modifiedBy;
+    @Setter @Column(nullable = false, length = 500) private String title;
+    @Setter @Column(nullable = false, length = 10000) private String content;
 
+    @Setter private String hashTag;
+
+    @ToString.Exclude //순환참조를 발생시킬 수 있어서 제외시킨다.
+    @OrderBy("id")
+    @OneToMany(mappedBy = "article", cascade = CascadeType.ALL)
+    private final Set<ArticleComment> articleComments = new LinkedHashSet<>();
+
+    @CreatedDate @Column(nullable = false) private LocalDateTime createdAt;
+    @CreatedBy @Column(nullable = false, length = 100) private String createdBy;
+    @LastModifiedDate @Column(nullable = false) private LocalDateTime modifiedAt;
+    @LastModifiedBy @Column(nullable = false, length = 100) private String modifiedBy;
+
+    private Article(String title, String content, String hashTag) {
+        this.title = title;
+        this.content = content;
+        this.hashTag = hashTag;
+    }
+
+    //Factory Method
+    public static Article of(String title, String content, String hashTag) {
+        return new Article(title, content, hashTag);
+    }
+
+    //equals와 hashCode는 항상 함께 override되어야 한다.
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Article)) return false;
+        Article article = (Article) o;
+        return id != null && id.equals(article.id); //영속화 되지 않은 엔티티는 다른값으로 취급한다.
+    }
+
+    //해당 메모리 주소값을 반환한다.
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
--- src/main/java/com/study/board/domain/ArticleComment.java
+++ src/main/java/com/study/board/domain/ArticleComment.java
@@ -1,17 +1,60 @@
 package com.study.board.domain;
 
-import javax.persistence.Entity;
+import lombok.*;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 @Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString
+@Table(indexes = {
+        @Index(columnList = "content"),
+        @Index(columnList = "createdAt"),
+        @Index(columnList = "createdBy"),
+})
+@EntityListeners(AuditingEntityListener.class)
 public class ArticleComment {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private Article article;
-    private String content;
 
-    private LocalDateTime createdAt;
-    private String createdBy;
-    private LocalDateTime modifiedAt;
-    private String modifiedBy;
+    @ManyToOne(optional = false) @Setter private Article article;
+    @Setter @Column(nullable = false, length = 500) private String content;
+
+    @CreatedDate @Column(nullable = false) private LocalDateTime createdAt;
+    @CreatedBy @Column(nullable = false, length = 100) private String createdBy;
+    @LastModifiedDate @Column(nullable = false) private LocalDateTime modifiedAt;
+    @LastModifiedBy @Column(nullable = false, length = 100) private String modifiedBy;
+
+    private ArticleComment(Article article, String content) {
+        this.article = article;
+        this.content = content;
+    }
+
+    //Factory Method
+    public static ArticleComment of(Article articles, String content) {
+        return new ArticleComment(articles, content);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof ArticleComment)) return false;
+        ArticleComment that = (ArticleComment) o;
+        return id != null && id.equals(that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
